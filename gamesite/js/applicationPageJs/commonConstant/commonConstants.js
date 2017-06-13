06e1f2223f00ffe5f17dcce9fdc0be57b64cfdcb
kerry <2170510391@qq.com>
2017-06-13 14:40:29 +0800

    updatedrop

--- gamesite/js/applicationPageJs/commonConstant/commonConstants.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Administrator on 2017/5/16.
- */
-
-
-function AjaxErrorMessage() {
-    var ErrorMessagemes="网络访问失败";
-    return ErrorMessagemes;
-}
--- gamesite/js/pageLayoutJs/common/commonPageLayout.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Administrator on 2017/5/10.
- */
-
-//公共的跳转模块
-
-function commonSkipPageModule(){
-    skipPageModule();
-}
